0e6b7b7775a056dfc1d926fd5813e5d8ed370899
Edward Feng <feng.dingding@gmail.com>
2020-11-11 09:13:21 -0500

    Swap Linux 2/3 after review

--- linux-1/ssh/secret/yes.c
+++ linux-1/ssh/secret/yes.c
@@ -7,10 +7,7 @@ int main()
     setbuf(stderr, NULL);
 
     puts("You got it!");
-    puts("bcactf{2-ch005inG_7h3_c0rr3c7_b1n4ry}");
+    puts("bcactf{3-ch005inG_7h3_c0rr3c7_b1n4ry}");
     puts("I wonder what would happen if you snuck a malicious program into the PATH?");
     puts("Hmm... it's something to think about.");
-    puts("Oh, can you also help me with one more thing?");
-    puts("I think someone's running `some-program-that-takes-arguments`.");
-    puts("Can you see what they're up to? Thanks.");
 }
