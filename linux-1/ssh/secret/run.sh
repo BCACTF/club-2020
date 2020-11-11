1f133566d34016252fde166ffc561a8006ad1c21
Edward Feng <feng.dingding@gmail.com>
2020-11-10 20:41:11 -0500

    8

--- linux-1/ssh/secret/run.sh
+++ linux-1/ssh/secret/run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-/bin/some-program-that-takes-arguments bcactf{3-pr0c3s5e5_c4n_l3aK_1nf0rMa7i0N} &
+nohup /usr/bin/some-program-that-takes-arguments bcactf{3-pr0c3s5e5_c4n_l3aK_1nf0rMa7i0N} &
 /usr/sbin/sshd -D -e
