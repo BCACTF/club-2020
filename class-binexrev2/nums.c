0979f8488dda1391527e0db95f23bd944bd30da8
Edward Feng <ed@ewf.me>
2021-01-27 14:31:09 -0500

    Add dynamic binrev

---
+++ class-binexrev2/nums.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    int a = 3;
+    int b = 5;
+    int c = a + b;
+    int d = 2 * a - b;
+    printf("The value of c is: %d\n", c);
+    printf("The value of d is: %d\n", d);
+}
